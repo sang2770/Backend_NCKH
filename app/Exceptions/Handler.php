bdfb770badbcccfd637e20f56c626a9f835a6df2
Nguyen Sang <nguyenvansang30102001@gmail.com>
2022-01-06 15:46:54 +0700

    add err auth

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -37,5 +38,13 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    // Check Auth
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['status' => 'Failed', 'Err_Message' => 'Unauthenticated.'], 401);
+        }
     }
 }
