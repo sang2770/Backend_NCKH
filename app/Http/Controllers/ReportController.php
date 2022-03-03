622ba6cba75528b38774f93bbf7ab5cb152fe918
Nguyen Sang <nguyenvansang30102001@gmail.com>
2022-03-03 22:10:01 +0700

    Fix

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -100,7 +100,7 @@ class ReportController extends Controller
     // Thống kê cập nhật
     private function LogicUpdateReport($request)
     {
-        $list = Tb_lichsu::where('Tb_lichsu.MaSinhVien', $request->MaSinhVien);
+        $list = Tb_lichsu::where('Tb_LichSu.MaSinhVien', $request->MaSinhVien);
             
             if($list->count()==0)
             {
@@ -120,9 +120,9 @@ class ReportController extends Controller
                 $list =$list->whereYear('ThoiGian', $request->Nam);
             }
             $list=$list
-                ->join('Tb_tk_quanly', 'Tb_tk_quanly.MaTK', '=', 'Tb_lichsu.MaTK')
-                ->join('tb_sinhvien', 'tb_sinhvien.MaSinhVien', '=', 'Tb_lichsu.MaSinhVien')
-                ->select('NoiDung', 'TenDangNhap', 'ThoiGian', 'Tb_lichsu.MaSinhVien');
+                ->join('Tb_tk_quanly', 'Tb_tk_quanly.MaTK', '=', 'Tb_LichSu.MaTK')
+                ->join('tb_sinhvien', 'tb_sinhvien.MaSinhVien', '=', 'Tb_LichSu.MaSinhVien')
+                ->select('NoiDung', 'TenDangNhap', 'ThoiGian', 'Tb_LichSu.MaSinhVien');
             return $list;           
     }
     public function ExportUpdate(Request $request)
