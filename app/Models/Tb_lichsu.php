432638d8ada047a4469fb343958e857d2c99b2a6
Nguyen Sang <nguyenvansang30102001@gmail.com>
2022-03-03 22:16:28 +0700

    Fix

--- app/Models/Tb_lichsu.php
+++ app/Models/Tb_lichsu.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tb_lichsu extends Model
 {
 
-    protected $table = 'Tb_lichsu';
+    protected $table = 'Tb_LichSu';
     protected $primaryKey = 'ID';
     protected $fillable = [
         'ID',
